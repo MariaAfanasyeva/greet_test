246e7231cf2bcb6d525087860bf621d107f36540
maryia <masha.afanaseva.29@mail.ru>
2021-04-08 14:22:13 +0300

    Added input validation

--- greet.py
+++ greet.py
@@ -2,9 +2,12 @@ def greet(name=None):
     if name is None:
         string = 'Hello, my friend'
     else:
-        if name.isupper():
-            string = 'HELLO, ' + name
+        if isinstance(name, str):
+            if name.isupper():
+                string = 'HELLO, ' + name
+            else:
+                string = 'Hello, ' + name
         else:
-            string = 'Hello, ' + str(name)
+            string = 'Wrong input'
     return string
 
--- greet_test.py
+++ greet_test.py
@@ -4,6 +4,8 @@ from greet import greet
 
 def test_greet():
     assert greet("Vasya") == "Hello, Vasya"
+    assert greet(54) == "Wrong input"
+    assert greet(3.5) == "Wrong input"
 
 
 def test_empty_greet():
