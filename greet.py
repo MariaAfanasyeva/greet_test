90d246f3271209f81e6bcd600795dfccc3cad145
maryia <masha.afanaseva.29@mail.ru>
2021-04-08 17:11:03 +0300

    change input validation

--- greet.py
+++ greet.py
@@ -1,20 +1,19 @@
 def greet(*names):
     if len(names) == 0:
         string = 'Hello, my friend'
-    elif len(names) == 1:
-        if isinstance(names[0], str):
+        return string
+    else:
+        for name in names:
+            if not isinstance(name, str):
+                string = 'Wrong input'
+                return string
+        if len(names) == 1:
             if names[0].isupper():
                 string = 'HELLO, ' + names[0]
             else:
                 string = 'Hello, ' + names[0]
         else:
-            string = 'Wrong input'
-    elif len(names) == 2:
-        for name in names:
-            if not isinstance(name, str):
-                string = 'Wrong input'
-                return string
-        string = 'Hello, ' + names[0] + ' and ' + names[1]
+            string = 'Hello, ' + names[0] + ' and ' + names[1]
     return string
 
 
