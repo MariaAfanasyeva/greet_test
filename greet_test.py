8028fb8d2415c7b43502397a3f75a54541683a19
maryia <masha.afanaseva.29@mail.ru>
2021-04-09 13:27:53 +0300

    Added parametrize

--- greet_test.py
+++ greet_test.py
@@ -2,8 +2,9 @@ import pytest
 from greet import greet
 
 
-def test_greet():
-    assert greet("Vasya") == "Hello, Vasya"
+@pytest.mark.parametrize("test_input, expected", [("Vasya", "Hello, Vasya"), (54, "Wrong input"), (3.5, "Wrong input")])
+def test_greet(test_input, expected):
+    assert greet(test_input) == expected
 
 
 def test_empty_greet():
@@ -14,8 +15,7 @@ def test_isupper():
     assert greet("HARRY") == "HELLO, HARRY"
 
 
-def test_two_names():
-    assert greet('John', 'Any') == "Hello, John and Any"
-    assert greet('John', 4) == "Wrong input"
-    assert greet(5) == "Wrong input"
-
+@pytest.mark.parametrize("expected, test_input1, test_input2", [("Hello, John and Any", "John", "Any"), ("Wrong input",
+                                                                                                         "John", 4)])
+def test_two_names(expected, test_input1, test_input2):
+    assert greet(test_input1, test_input2) == expected
